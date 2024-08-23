173e2efb278522cf49de3ffbf5877b6a6e9dfe19
Garcia02 <jefersongarcia.crazy@hotmail.com>
2024-08-23 16:07:58 -0300

    Create database.py

---
+++ database.py
@@ -0,0 +1,19 @@
+import mysql.connector
+
+def get_connection():
+    try:
+        conexao = mysql.connector.connect(
+            host="127.0.0.1",
+            user="root",
+            database="cadastro"
+        )
+        return conexao
+    except mysql.connector.Error as erro:
+        print(f"Erro ao conectar ao MySQL: {erro}")
+        return None
+
+def close_connection(conexao, cursor=None):
+    if cursor:
+        cursor.close()
+    if conexao.is_connected():
+        conexao.close()
